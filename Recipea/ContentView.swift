f5ed2d8dd4bf9670db0839fae77e8ef029e017f3
Orri <orrmann@gmail.com>
2023-08-25 18:36:17 +0300

    Initial Commit

---
+++ Recipea/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Recipea
+//
+//  Created by Orri Arnórsson on 25.8.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
